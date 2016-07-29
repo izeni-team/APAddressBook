6e0089eeec5f9ad7d514339f6e1486c57ab03ae8
Bryan Henderson <bhenderson@izeni.com>
2016-07-29 17:48:08 -0600

    Added modulemap to prevent conflict with APAddressBook.h.

---
+++ APAddressBook/APAddressBook-umbrella.h
@@ -0,0 +1,9 @@
+//
+//  APAddressBook-umbrella.h
+//  APAddressBook
+//
+//  Created by Christopher Bryan Henderson on 7/29/16.
+//  Copyright © 2016 Afterplay. All rights reserved.
+//
+
+#import "APAddressBook-Bridging.h"
